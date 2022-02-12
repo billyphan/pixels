95b40a3030b087a208b55f477c40494bfcddfdf8
Billy Phan <billyphann@gmail.com>
2022-02-13 00:06:30 +1100

    Connect to postgresql

--- backend/src/main/java/com/billynixoncorp/bestprojectever/controller/ExampleController.java
+++ backend/src/main/java/com/billynixoncorp/bestprojectever/controller/ExampleController.java
@@ -1,20 +1,60 @@
 package com.billynixoncorp.bestprojectever.controller;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.List;
+
 
 import com.billynixoncorp.bestprojectever.contract.ExampleResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.billynixoncorp.bestprojectever.dao.Customer;
 
 @RestController
 public class ExampleController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private static final Logger log = LoggerFactory.getLogger(ExampleController.class);
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @GetMapping("/exampleendpoint")
     public ExampleResource greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new ExampleResource(counter.incrementAndGet(), String.format(template, name));
+    }
+    @GetMapping("/exampleendpointr")
+    public ExampleResource greetingr(@RequestParam(value = "name", defaultValue = "World") String nameq) {
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE IF EXISTS customers");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> log.info(customer.toString()));
+
+        return new ExampleResource(counter.incrementAndGet(), String.format(template, nameq));
     }
 }
